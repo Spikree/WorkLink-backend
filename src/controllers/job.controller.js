76bea1a68140642ee3cbc7c37fdb0529822f21d6
spikree <avimahalingpure10@gmail.com>
2025-03-22 23:41:51 +0530

    add get jobs method

--- src/controllers/job.controller.js
+++ src/controllers/job.controller.js
@@ -1,69 +1,102 @@
 import Job from "../models/job.model.js";
-import Applications from "../models/application.model.js"
+import Applications from "../models/application.model.js";
+import User from "../models/user.model.js";
 
-export const createJob = async(req,res) => {
-    const {user} = req.user;
-    const {title, description,budget,skillsRequired,status} = req.body;
+export const createJob = async (req, res) => {
+  const { user } = req.user;
+  const { title, description, budget, skillsRequired, status } = req.body;
 
-    if(!title || !description || !budget || !skillsRequired) {
-        return res.status(400).json({message: "All Fields Are Required"});
-    };
+  if (!title || !description || !budget || !skillsRequired) {
+    return res.status(400).json({ message: "All Fields Are Required" });
+  }
 
-    try {
-        const job = await Job.create({
-            title: title,
-            description: description,
-            budget: budget,
-            skillsRequired:skillsRequired,
-            status: status,
-            employer: user._id
-        });
+  try {
+    const job = await Job.create({
+      title: title,
+      description: description,
+      budget: budget,
+      skillsRequired: skillsRequired,
+      status: status,
+      employer: user._id,
+    });
 
-        job.save();
+    job.save();
 
-        return res.status(200).json({
-            message: "Job Listing Created Sucessfully",
-            job
-        })
-    } catch (error) {
-        console.log("error in job controller at create job" + error);
-        return res.status(500).json({
-            message: "Internal Server Error"
-        });
-    }
+    return res.status(200).json({
+      message: "Job Listing Created Sucessfully",
+      job,
+    });
+  } catch (error) {
+    console.log("error in job controller at create job" + error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 };
 
-export const getJobApplications = async (req,res) => {
-    const {id: jobId} = req.params;
-    const {user} = req.user;
+export const getJobApplications = async (req, res) => {
+  const { id: jobId } = req.params;
+  const { user } = req.user;
 
-    if(!jobId) {
-        return res.status(400).json({
-            message: "Job Id is Required"
-        })
+  if (!jobId) {
+    return res.status(400).json({
+      message: "Job Id is Required",
+    });
+  }
+
+  try {
+    const applications = await Applications.find({ job: jobId }).lean();
+
+    const job = await Job.findById(jobId);
+
+    const isCurrentEmployer = job.employer.toString() === user._id;
+
+    if (!isCurrentEmployer) {
+      return res.status(401).json({
+        message: "This Job Listing Is Not By You",
+      });
     }
 
-    try {
-        const applications = await Applications.find({job: jobId}).lean();
+    return res.status(200).json({
+      message: "Fetched All Job Applications Sucessfully",
+      applications,
+    });
+  } catch (error) {
+    console.log("error in job controller in get job applications" + error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
 
-        const job = await Job.findById(jobId)
+export const getJobs = async (req, res) => {
+  try {
+    const jobs = await Job.find({ status: "open" });
 
-        const isCurrentEmployer = job.employer.toString() === user._id;
+    const employerIds = jobs.map((job) => job.employer);
 
-        if(!isCurrentEmployer) {
-            return res.status(401).json({
-                message: "This Job Listing Is Not By You"
-            })
-        }
+    const employers = await User.find({ _id: { $in: employerIds } });
 
-        return res.status(200).json({
-            message: "Fetched All Job Applications Sucessfully",
-            applications
-        })
-    } catch (error) {
-        console.log("error in job controller in get job applications"+error);
-        return res.status(500).json({
-            message: "Internal Server Error"
-        })
-    }
-}
+    const employerMap = {};
+
+    employers.forEach((employer) => {
+      employerMap[employer._id.toString()] =
+        employer.profile?.name || "Unknown";
+    });
+
+    const jobsWithEmployerNames = jobs.map((job) => ({
+        ...job.toObject(),
+        employerName: employerMap[job.employer.toString()] || "Unknown",
+    }));
+
+    return res.status(200).json({
+        message: "Fetched All Jobs Sucessfully",
+        jobs: jobsWithEmployerNames
+    })
+  } catch (error) {
+    console.log("Error in job controller at getJobs" + error);
+    return res.status(500).json({
+        message : "Internal Server Error"
+    })
+  }
+};
--- src/routes/job.route.js
+++ src/routes/job.route.js
@@ -1,11 +1,13 @@
 import express from "express";
 import verifyToken from "../middleware/verifytoken.middleware.js";
 import checkEmployerRole from "../middleware/checkEmployerRole.middleware.js";
-import { createJob, getJobApplications } from "../controllers/job.controller.js";
+import { createJob, getJobApplications, getJobs } from "../controllers/job.controller.js";
+import checkFreelancerRole from "../middleware/checkFreelancerRole.middleware.js";
 
 const router = express.Router();
 
 router.post("/createJob", verifyToken,checkEmployerRole,createJob);
-router.get("/getApplications/:id",verifyToken,checkEmployerRole,getJobApplications)
+router.get("/getJobs",verifyToken,checkFreelancerRole,getJobs);
+router.get("/getApplications/:id",verifyToken,checkEmployerRole,getJobApplications);
 
 export default router;
