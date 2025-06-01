666e398287065be709f7aa2b83a19f63659fabee
spikree <avimahalingpure10@gmail.com>
2025-06-01 21:18:09 +0530

    upload profile picture route

--- src/routes/profile.route.js
+++ src/routes/profile.route.js
@@ -1,14 +1,27 @@
 import express from "express";
 import verifyToken from "../middleware/verifytoken.middleware.js";
-import {editUser, getEmployer, getUser, getUserProfile,getUserById} from "../controllers/profile.controller.js";
+import upload from "../middleware/multer.middleware.js";
+import {
+  editUser,
+  getEmployer,
+  getUser,
+  getUserProfile,
+  getUserById,
+  uploadProfilePicture,
+} from "../controllers/profile.controller.js";
 
 const router = express.Router();
 
-router.get("/getUser",verifyToken,getUser);
-router.put("/edit",verifyToken,editUser);
-router.get("/getUserProfile/:userId",verifyToken,getUserProfile);
-router.get("/getEmployer/:id",verifyToken,getEmployer);
-router.get("/getUserById/:id",verifyToken,getUserById);
+router.get("/getUser", verifyToken, getUser);
+router.put("/edit", verifyToken, editUser);
+router.get("/getUserProfile/:userId", verifyToken, getUserProfile);
+router.get("/getEmployer/:id", verifyToken, getEmployer);
+router.get("/getUserById/:id", verifyToken, getUserById);
+router.post(
+  "/uploadProfilePicture",
+  verifyToken,
+  upload.single("image"),
+  uploadProfilePicture
+);
 
 export default router;
-
